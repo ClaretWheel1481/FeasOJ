a7ef11a504076ad226cb35c144dcc1337731cfc0
ClaretWheel1481 <245922575@qq.com>
2024-05-12 20:46:10 +0800

    backup

--- backend-src/src/main.go
+++ backend-src/src/main.go
@@ -35,11 +35,5 @@ func initSql() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	fmt.Println("Server running on http://localhost:8080")
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
