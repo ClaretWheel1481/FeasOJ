b7b2fd0cb1ea328fe6c5f60fe06a15b46256aae0
ClaretWheel1481 <245922575@qq.com>
2024-05-13 12:04:34 +0800

    用户表初始化方法完成、注册获取验证码按钮添加。

--- backend-src/main.go
+++ backend-src/main.go
@@ -1,38 +1,39 @@
-package backendsrc
+package main
 
 import (
-	"database/sql"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-// 连接数据库、创建表
 // 用户表：uid, avartar, username, password, email, score, synopsis, submit_history
-func initSql() {
-	//连接MySQL数据库
-	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+type User struct {
+	Uid           int    `gorm:"primaryKey"`
+	Avartar       string `gorm:"not null"`
+	Username      string `gorm:"not null"`
+	Password      string `gorm:"not null"`
+	Email         string `gorm:"not null"`
+	Score         int    `gorm:"not null"`
+	Synopsis      string `gorm:"not null"`
+	SubmitHistory string `gorm:"not null"`
+}
 
-	//创建表
-	sqlCreateTable := `
-	    CREATE TABLE IF NOT EXISTS user (
-	        uid INT AUTO_INCREMENT PRIMARY KEY,
-			avartar VARCHAR(255),
-			username VARCHAR(24),
-			password VARCHAR(32),
-			email VARCHAR(255),
-			score INT,
-			synopsis VARCHAR(255),
-			submit_history VARCHAR(255)
-	    )
-	`
-	_, err = db.Exec(sqlCreateTable)
+// 连接数据库、创建表
+func initSql(db *gorm.DB) {
+	fmt.Println("初始化数据库中...")
+	db.AutoMigrate(&User{})
+	fmt.Println("初始化数据库成功。")
 }
 
 func main() {
-	r := gin.Default()
-	r.Run() // listen and serve on 0.0.0.0:8080
+	// r := gin.Default()
+	fmt.Println("连接数据库中...")
+	db, err := gorm.Open(mysql.Open("FeasOJ:YE8cimwfC5KxpN6N@tcp(cloud.claret.space:9936)/FeasOJ?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("数据库连接失败。")
+	}
+	fmt.Println("数据库连接成功。")
+	initSql(db)
+	// r.Run() // listen and serve on 0.0.0.0:8080
 }
--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -38,8 +38,13 @@ const TinyIconMailContent = iconMailContent()
                 <div>
                     <h3>邮箱验证码</h3>
                 </div>
-                <div class="input">
-                    <tiny-input v-model="verificationcode" placeholder="点击右侧按钮获取验证码"></tiny-input>
+                <div>
+                    <div class="input" style="display: inline-block; margin-left: 80px;">
+                        <tiny-input v-model="verificationcode" placeholder="点击右侧按钮获取验证码"></tiny-input>
+                    </div>
+                    <div style="display: inline-block;">
+                        <tiny-button type="text">获取验证码</tiny-button>
+                    </div>
                 </div>
                 <!-- TODO:右侧“获取验证码”按钮 -->
                 <div>
