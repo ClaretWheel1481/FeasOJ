8a11c627c6f7af55234a44ea68dea805bc0c7535
ClaretWheel1481 <245922575@qq.com>
2024-05-14 14:05:52 +0800

    修改后端连接形式以及代码结构

--- backend-src/main.go
+++ backend-src/main.go
@@ -37,9 +37,14 @@ type Problem struct {
 }
 
 func main() {
-	initSql()
+	if initSql() {
+		fmt.Println("[FeasOJ]数据库初始化成功！")
+	} else {
+		fmt.Println("[FeasOJ]数据库初始化失败，请确认数据库连接是否正确！")
+		return
+	}
 	// 启动服务器
-	// gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	fmt.Println("[FeasOJ]启动服务器中...")
 	//TODO:响应前端部分功能待实现
@@ -66,6 +71,6 @@ func main() {
 	}
 
 	fmt.Println("服务器启动成功，API地址：http://localhost:37881/api/")
-
+	fmt.Println("若要修改数据库连接信息，请修改config.xml文件.")
 	r.Run("0.0.0.0:37881")
 }
--- backend-src/sql.go
+++ backend-src/sql.go
@@ -1,21 +1,91 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// 连接数据库、创建表
-func initSql() {
-	fmt.Println("[FeasOJ]连接数据库中...")
-	db, err := gorm.Open(mysql.Open("FeasOJ:YE8cimwfC5KxpN6N@tcp(cloud.claret.space:9936)/FeasOJ?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+type Config struct {
+	DbName     string `xml:"dbname"`
+	DbUser     string `xml:"dbuser"`
+	DbPassword string `xml:"dbpassword"`
+	DbAddress  string `xml:"dbaddress"`
+	DbPort     string `xml:"dbport"`
+}
+
+func writeConfig(dbName, dbUser, dbPassword, dbAddress string) error {
+	// 创建配置文件
+	config := Config{
+		DbName:     dbName,
+		DbUser:     dbUser,
+		DbPassword: dbPassword,
+		DbAddress:  dbAddress,
+	}
+	configXml, err := xml.Marshal(config)
 	if err != nil {
-		fmt.Println("[FeasOJ]数据库连接失败。")
+		return err
+	}
+	err = os.WriteFile("config.xml", configXml, 0644)
+	return err
+}
+
+// 连接数据库、创建表
+func initSql() bool {
+	//判断是否有config.xml文件，没有则输入
+	if _, err := os.Stat("config.xml"); os.IsNotExist(err) {
+		fmt.Println("[FeasOJ]请输入数据库连接信息：")
+		fmt.Print("数据库名：")
+		var dbName string
+		fmt.Scanln(&dbName)
+		fmt.Print("数据库用户名：")
+		var dbUser string
+		fmt.Scanln(&dbUser)
+		fmt.Print("数据库密码：")
+		var dbPassword string
+		fmt.Scanln(&dbPassword)
+		fmt.Print("数据库地址（加上端口）：")
+		var dbAddress string
+		fmt.Scanln(&dbAddress)
+		fmt.Println("[FeasOJ]正在保存数据库连接信息...")
+		// 将连接信息作为xml保存到目录
+		err := writeConfig(dbName, dbUser, dbPassword, dbAddress)
+		if err != nil {
+			fmt.Println("[FeasOJ]保存失败，请检查权限。")
+			return false
+		}
+	}
+
+	// 读取config.xml文件并赋值
+	configFile, err := os.Open("config.xml")
+	if err != nil {
+		return false
+	}
+	defer configFile.Close()
+	var config Config
+	err = xml.NewDecoder(configFile).Decode(&config)
+	if err != nil {
+		return false
+	}
+	dbName := config.DbName
+	dbUser := config.DbUser
+	dbPassword := config.DbPassword
+	dbAddress := config.DbAddress
+
+	fmt.Println("[FeasOJ]连接数据库中...")
+
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbAddress + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return false
 	}
 	fmt.Println("[FeasOJ]数据库连接成功。")
 	fmt.Println("[FeasOJ]创建数据表中...")
 	db.AutoMigrate(&User{}, &Problem{})
 	fmt.Println("[FeasOJ]创建数据表成功。")
+	return true
 }
