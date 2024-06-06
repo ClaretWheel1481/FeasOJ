4a86c358c477d80a8edbea429c2a02a76e2d08b6
ClaretWheel1481 <245922575@qq.com>
2024-06-06 22:48:40 +0800

    移除无用包

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ViteMonacoPlugin from 'vite-plugin-monaco-editor'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
-    ViteMonacoPlugin,
+    legacy()
   ],
   define: {
     'process.env': { ...process.env }
