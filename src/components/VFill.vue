a630d7d961a8d61ea9c45688c34893c59824ad4c
ClaretWheel1481 <245922575@qq.com>
2024-08-31 13:28:02 +0800

    chore: 前端依赖更新

--- src/components/VFill.vue
+++ src/components/VFill.vue
@@ -2,6 +2,8 @@
 import { VFooter } from 'vuetify/lib/components/index.mjs';
 </script>
 
+<!-- 若需要将站点部署于中国境内服务器，请修改此组件，并在App.vue中启用该组件！ -->
+ 
 <template>
   <v-footer>
     <p style="font-size: 12px">闽ICP备*******</p>
