a1a8322da4792dc85468fd060e2d3849432fff6a
ClaretWheel1481 <LanceHuang245@outlook.com>
2025-01-14 02:10:45 +0800

    mod: 为sse服务添加语言参数，完善后端的i18n服务

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -4,7 +4,8 @@ import { i18n } from '../plugins/vue-i18n';
 // sse
 export function initSSE(uid, callback) {
     function connect() {
-        const eventSource = new EventSource(apiUrl + '/notification/' + uid);
+        const language = i18n.global.locale; // 获取当前使用的语言
+        const eventSource = new EventSource(`${apiUrl}/notification/${uid}?lang=${language.value}`);
 
         eventSource.onmessage = function (event) {
             callback(event.data);
